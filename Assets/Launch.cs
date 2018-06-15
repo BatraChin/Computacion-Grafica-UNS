000f46f89f3084da37f5a01fdb456d5da49617b9
BatraChin <guillef92@gmail.com>
2018-06-15 06:18:32 -0300

    3,2,1, Despegue!

---
+++ Assets/Launch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour {
+
+
+	public Rigidbody rb;
+	public	KeyCode vkey;
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+					Debug.Log(vkey);
+
+		if(Input.GetKey("space")){
+			rb.AddForce(0,0.2f,0,ForceMode.Impulse);
+		}
+	}
+}
