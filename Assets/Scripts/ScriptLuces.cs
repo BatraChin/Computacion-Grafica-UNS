719175ebfb893c8922fe00a95c0c462d48e94e71
BatraChin <guillef92@gmail.com>
2018-06-21 22:30:03 -0300

    No se que hice

---
+++ Assets/Scripts/ScriptLuces.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptLuces : MonoBehaviour {
+
+	// Use this for initialization
+	//public Light myLight=GetComponent<Light>();
+
+	/*IEnumerator MyCoroutine()
+    {
+        //This is a coroutine
+        
+       
+         yield return new WaitForSeconds(1);    //Wait one frame
+	}*/
+	void Start () {
+	/*	int turn=1;
+		while (true) {
+        MyCoroutine();
+		if(turn==1){
+        	myLight.intensity =0;
+			turn==0;
+		}
+		else{
+			turn=1;
+			myLight.intensity=2;
+		}
+    	}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
