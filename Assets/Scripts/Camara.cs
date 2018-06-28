41b4d8be3f0a06fe543c9ec7917b2577a8eaddeb
BatraChin <guillef92@gmail.com>
2018-06-28 19:51:05 -0300

    muchas cosas, camaras, efectos, etc

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -4,16 +4,70 @@ using UnityEngine;
 
 public class Camara : MonoBehaviour {
 
-	public GameObject player;
+	public Camera MainCamera;
+	public Camera CameraDisplay2;
+	public Camera CameraDisplay3;
+	public Camera CameraDisplay4;
+	public Camera CameraDisplay5;
 	// Use this for initialization
 	void Start () {
+
+
+		MainCamera=GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+		CameraDisplay2=GameObject.FindWithTag("CameraDisplay2").GetComponent<Camera>();
+		CameraDisplay3=GameObject.FindWithTag("CameraDisplay3").GetComponent<Camera>();
+		CameraDisplay4=GameObject.FindWithTag("CameraDisplay4").GetComponent<Camera>();
+		CameraDisplay5=GameObject.FindWithTag("CameraDisplay5").GetComponent<Camera>();
+		
 		
 	}
 	
-	public Transform target;
+	public Transform player;
     
     void Update ()
     {
-        transform.LookAt(target);
+        transform.LookAt(player);
     }
+
+	void FixedUpdate(){
+		if(Input.GetKey("1")){
+			MainCamera.enabled=true;
+			
+			CameraDisplay2.enabled=false;
+			CameraDisplay3.enabled=false;
+			CameraDisplay4.enabled=false;
+			CameraDisplay5.enabled=false;
+		}
+		if(Input.GetKey("2")){
+			
+			CameraDisplay3.enabled=false;
+			CameraDisplay4.enabled=false;
+			MainCamera.enabled=false;	
+			CameraDisplay2.enabled=true;
+			CameraDisplay5.enabled=false;
+		}
+		if(Input.GetKey("3")){
+			CameraDisplay3.enabled=true;
+			CameraDisplay4.enabled=false;
+			MainCamera.enabled=false;	
+			CameraDisplay2.enabled=false;	
+			CameraDisplay5.enabled=false;	
+		}
+		if(Input.GetKey("4")){
+			CameraDisplay3.enabled=false;
+			CameraDisplay4.enabled=true;
+			MainCamera.enabled=false;	
+			CameraDisplay2.enabled=false;
+			CameraDisplay5.enabled=false;		
+		}
+		if(Input.GetKey("5")){
+			CameraDisplay5.enabled=true;
+
+			
+			MainCamera.enabled=false;
+			CameraDisplay2.enabled=false;
+			CameraDisplay3.enabled=false;
+			CameraDisplay4.enabled=false;
+		}
+	}
 }
